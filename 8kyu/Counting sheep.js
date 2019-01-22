16c926da6c07e95df174e3a620a7d291a2131174
GuppyTheCat <46162723+GuppyTheCat@users.noreply.github.com>
2019-01-22 14:25:10 +0200

    upload

---
+++ 8kyu/Counting sheep.js
@@ -0,0 +1,28 @@
+/*
+https://www.codewars.com/kata/counting-sheep-dot-dot-dot/
+
+Consider an array of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined
+*/
+function countSheeps(arrayOfSheep) {
+  var sum = 0;
+    for (var i = 0; i <= arrayOfSheep.length; i++)
+    {
+      if (arrayOfSheep[i] == true)
+      {
+      sum = sum + 1;
+      }
+    }
+    return sum;
+}
