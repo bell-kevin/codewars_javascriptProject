4e722c965a46d43067ba4819680d236808aee292
GuppyTheCat <46162723+GuppyTheCat@users.noreply.github.com>
2019-01-22 12:38:19 +0200

    upload

---
+++ 8kyu/Opposite number.js
@@ -0,0 +1,9 @@
+/*
+https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+Very simple, given a number, find its opposite.
+*/
+function opposite(number) {
+  number = number * (-1);
+  return number;
+}
---
+++ 8kyu/Return Negative.js
@@ -0,0 +1,16 @@
+/*
+https://www.codewars.com/kata/55685cd7ad70877c23000102
+
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+Notes:
+The number can be negative already, in which case no change is required.
+Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+*/
+function makeNegative(num) {
+  if (num > 0)
+  {
+  num = num * (-1);
+  }
+return num;
+}
