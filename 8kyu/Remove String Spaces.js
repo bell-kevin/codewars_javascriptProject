0d9252f7baee6e06623df5f3da82b8d3d33cc123
GuppyTheCat <46162723+GuppyTheCat@users.noreply.github.com>
2019-01-27 01:29:31 +0200

    upload

---
+++ 8kyu/Remove String Spaces.js
@@ -0,0 +1,8 @@
+/*
+https://www.codewars.com/kata/remove-string-spaces/
+
+Simple, remove the spaces from the string, then return the resultant string.
+*/
+function noSpace(x){
+  return x.split(' ').join('');
+}
