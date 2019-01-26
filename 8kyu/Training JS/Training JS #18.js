fc0b2e73d1718f99e644dc1dd811683293030268
GuppyTheCat <46162723+GuppyTheCat@users.noreply.github.com>
2019-01-27 00:04:15 +0200

    upload

--- 8kyu/Training JS/Training JS #18.js
+++ 8kyu/Training JS/Training JS #18.js
@@ -3,10 +3,9 @@ Methods of String object--concat() split() and its good friend join()
 */
 function splitAndMerge(str,sp){
   var words = str.split(' ');
-  var sentence = [];
   for (var i=0; i<words.length; i++)
     {
-      sentence[i] = words[i].split('').join(sp);
+      words[i] = words[i].split('').join(sp);
     }
-  return sentence.join(' ');
+  return words.join(' ');
 }
