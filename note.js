09b70f9f25a997019aabc1f968fd643a54cc3db1
GuppyTheCat <46162723+GuppyTheCat@users.noreply.github.com>
2019-01-30 21:51:37 +0200

    upload

--- note.js
+++ note.js
@@ -1,2 +1,4 @@
 //Замена символов в строке. g for global, i for case sensitivity
 str.replace(/[aeiou]/gi,'');
+//Являются ли два объекта идентичными через Object.is
+isNegativeZero=n=>Object.is(n,-0);
