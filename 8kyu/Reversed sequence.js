824dd797837ee07ac3cae953a3e0b948d58ce170
GuppyTheCat <46162723+GuppyTheCat@users.noreply.github.com>
2019-02-01 10:39:22 +0200

    upload

---
+++ 8kyu/Reversed sequence.js
@@ -0,0 +1,16 @@
+/*
+https://www.codewars.com/kata/reversed-sequence/
+
+Get the number n (n>0) to return the reversed sequence from n to 1.
+
+Example : n=5 >> [5,4,3,2,1]
+*/
+reverseSeq = n => {
+ var arr=[];
+  while(n>0)
+    {
+      arr.push(n);
+      n--;
+    }
+  return arr;
+}
