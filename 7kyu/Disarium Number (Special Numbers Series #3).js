c75ff1cbac67acc014a5b83d2746d6c870c99311
GuppyTheCat <46162723+GuppyTheCat@users.noreply.github.com>
2019-02-03 23:00:49 +0200

    upload

---
+++ 7kyu/Disarium Number (Special Numbers Series #3).js
@@ -0,0 +1,30 @@
+/*
+https://www.codewars.com/kata/disarium-number-special-numbers-series-number-3/
+
+Definition
+Disarium number is the number that The sum of its digits powered with their respective positions is equal to the number itself.
+
+Task
+Given a number, Find if it is Disarium or not .
+
+Warm-up (Highly recommended)
+Playing With Numbers Series
+Notes
+Number passed is always Positive .
+Return the result as String
+Input >> Output Examples
+1- disariumNumber(89) ==> return "Disarium !!"
+Explanation:
+Since , 81 + 92 = 89 , thus output is "Disarium !!"
+2- disariumNumber(564) ==> return "Not !!"
+Explanation:
+Since , 51 + 62 + 43 = 105 != 546 , thus output is "Not !!"
+*/
+function disariumNumber(n){
+  var s=n+'',sum=0;
+  for(var i=0;i<s.length;i++)
+      sum+=s[i]**(i+1);
+  if (sum==n)
+    return "Disarium !!"
+  return "Not !!"
+}
