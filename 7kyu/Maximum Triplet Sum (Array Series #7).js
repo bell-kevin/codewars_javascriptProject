f42e3bc7cd952b564d599dcde629ccf1f23873ef
GuppyTheCat <46162723+GuppyTheCat@users.noreply.github.com>
2019-02-04 10:43:50 +0200

    upload

---
+++ 7kyu/Maximum Triplet Sum (Array Series #7).js
@@ -0,0 +1,43 @@
+/*
+https://www.codewars.com/kata/maximum-triplet-sum-array-series-number-7/
+
+Task
+Given an array/list [] of n integers , find maximum triplet sum in the array Without duplications .
+
+Notes :
+Array/list size is at least 3 .
+
+Array/list numbers could be a mixture of positives , negatives and zeros .
+
+Repeatition of numbers in the array/list could occur , So (duplications are not included when summing).
+
+Input >> Output Examples
+1- maxTriSum ({3,2,6,8,2,3}) ==> return (17)
+Explanation:
+As the triplet that maximize the sum {6,8,3} in order , their sum is (17)
+
+Note : duplications are not included when summing , (i.e) the numbers added only once .
+
+2- maxTriSum ({2,1,8,0,6,4,8,6,2,4}) ==> return (18)
+Explanation:
+As the triplet that maximize the sum {8, 6, 4} in order , their sum is (18) ,
+Note : duplications are not included when summing , (i.e) the numbers added only once .
+3- maxTriSum ({-7,12,-7,29,-5,0,-7,0,0,29}) ==> return (41)
+Explanation:
+As the triplet that maximize the sum {12 , 29 , 0} in order , their sum is (41) ,
+Note : duplications are not included when summing , (i.e) the numbers added only once .
+*/
+function maxTriSum(n){
+  var arr=n.sort((a,b)=>b-a),tr=0,c=0
+  for(var i=0;i<arr.length;i++)
+    {
+      if (i!=0)
+          if (arr[i-1]==arr[i])
+            continue
+      tr+=arr[i]
+      c++
+      if(c==3)
+        break
+    }
+  return tr
+}
--- 8kyu/Training JS/Training JS #27.js
+++ 8kyu/Training JS/Training JS #27.js
@@ -3,12 +3,12 @@ Training JS #27: methods of arrayObject---filter()
 */
 function countGrade(scores){
 var g={S:0, A:0, B:0, C:0, D:0, X:0};
-var arr=scores.filter(x=>
-   x==100           ? g.S++ :
-  (x>=90 && x<100)  ? g.A++ :
-  (x>=80 && x<90)   ? g.B++ :
-  (x>=60 && x<80)   ? g.C++ :
-  (x>=0 && x<60)    ? g.D++ :
-                      g.X++ )
+  scores.filter(x=>
+  x==100          ? g.S++ :
+  x>=90 && x<100  ? g.A++ :
+  x>=80 && x<90   ? g.B++ :
+  x>=60 && x<80   ? g.C++ :
+  x>=0 && x<60    ? g.D++ :
+                    g.X++ )
   return g;
 }
