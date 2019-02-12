ade843164e9ee25e5d293a33b47f0db367e98c12
GuppyTheCat <46162723+GuppyTheCat@users.noreply.github.com>
2019-02-12 16:58:13 +0200

    upload

---
+++ 7kyu/Binary Addition.js
@@ -0,0 +1,7 @@
+/*
+https://www.codewars.com/kata/binary-addition/
+
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+The binary number returned should be a string.
+*/
+addBinary=(a,b)=>(a+b).toString(2)
