21cdfcb6e8a73b75db6099be8767716e1382b8d5
GuppyTheCat <46162723+GuppyTheCat@users.noreply.github.com>
2019-02-15 06:13:11 +0200

    upload

---
+++ 6kyu/Your order, please.js
@@ -0,0 +1,28 @@
+/*
+https://www.codewars.com/kata/your-order-please/
+
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+*/
+function order(w){
+var r, res=[]
+w=w.split(' ')
+  for(var i=1;i<=w.length;i++)
+    {
+      r = new RegExp(i)
+      for(var j=0;j<w.length;j++)
+        {
+          if (r.test(w[j]))
+            res.push(w[j])
+        }
+    }
+  return res.join(' ')
+}
