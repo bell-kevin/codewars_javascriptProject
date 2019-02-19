6e77a711e7705135f5019c46feca1f11c1b3851c
GuppyTheCat <46162723+GuppyTheCat@users.noreply.github.com>
2019-02-19 17:17:08 +0200

    upload

---
+++ 7kyu/Sum of odd numbers.js
@@ -0,0 +1,29 @@
+/*
+https://www.codewars.com/kata/sum-of-odd-numbers/
+
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the row sums of this triangle from the row index (starting at index 1) e.g.:
+
+rowSumOddNumbers(1); // 1
+rowSumOddNumbers(2); // 3 + 5 = 8
+*/
+function rowSumOddNumbers(n) {
+  if (n <= 1) return n
+  var x = 1,
+      res
+  for (var i = 1; i < n; i++) {
+    res = 0
+    for (var j = 0; j <= i; j++) {
+      x += 2
+      res += x
+    }
+  }
+  return res
+}
