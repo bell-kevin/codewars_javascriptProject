40eaa8ad0a7a3122ecee6f747fdffe2395334e6e
GuppyTheCat <46162723+GuppyTheCat@users.noreply.github.com>
2019-02-20 18:16:14 +0200

    upload

---
+++ 8kyu/Remove exclamation marks.js
@@ -0,0 +1,6 @@
+/*
+https://www.codewars.com/kata/remove-exclamation-marks/
+
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+*/
+removeExclamationMarks = s => s.replace(/!/g, '')
