4f74ac64e6cf4a5e4a73adb976355eff297e0e0e
GuppyTheCat <46162723+GuppyTheCat@users.noreply.github.com>
2019-03-05 12:39:38 +0200

    upload

---
+++ 6kyu/Highest Scoring Word.js
@@ -0,0 +1,21 @@
+/*
+https://www.codewars.com/kata/highest-scoring-word/
+
+Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to it's position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+*/
+function high(x) {
+  if (x.trim() == '') return ''
+  var sums = x.split(' ')
+              .map(word => word.split('')
+              .map(letters => letters.charCodeAt(0) - 96)
+              .reduce((c, d) => c + d))
+  return x.split(' ')[sums.indexOf(Math.max(...sums))]
+}
