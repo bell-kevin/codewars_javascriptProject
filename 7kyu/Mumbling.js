bc39892ca318555042085629861808e78d52caf1
GuppyTheCat <46162723+GuppyTheCat@users.noreply.github.com>
2019-09-18 02:17:55 +0300

    remade in 1 line

--- 7kyu/Mumbling.js
+++ 7kyu/Mumbling.js
@@ -11,25 +11,26 @@ accum("cwAt") -> "C-Ww-Aaa-Tttt"
 The parameter of accum is a string which includes only letters from a..z and A..Z.
 */
 function accum(s) {
-  var str='';
-  for(var i=0;i<s.length;i++)
-    {
-      str+=s[i].toUpperCase();
-      for(var j=0;j<i;j++)
-        {
-          str+=s[i].toLowerCase();
+    var str = '';
+    for (var i = 0; i < s.length; i++) {
+        str += s[i].toUpperCase();
+        for (var j = 0; j < i; j++) {
+            str += s[i].toLowerCase();
         }
-      if(i<s.length-1)
-        str+='-';
+        if (i < s.length - 1)
+            str += '-';
     }
     return str;
 }
+
 //Alternative
 function accum(s) {
-  var str='';
-  for(var i=0;i<s.length;i++)
-    {
-      str+=s[i].toUpperCase()+s[i].toLowerCase().repeat(i)+((i<s.length-1)?'-':'');
+    var str = '';
+    for (var i = 0; i < s.length; i++) {
+        str += s[i].toUpperCase() + s[i].toLowerCase().repeat(i) + ((i < s.length - 1) ? '-' : '');
     }
     return str;
 }
+
+//Alternative
+accum = s => s.split("").map((item, i) => item.toUpperCase() + item.toLowerCase().repeat(i)).join("-");
