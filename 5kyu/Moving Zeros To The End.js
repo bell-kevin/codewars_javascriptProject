a06571dd8ff9d5d5b362de643f3ed54118032747
GuppyTheCat <46162723+GuppyTheCat@users.noreply.github.com>
2020-01-04 02:27:53 +0300

    Upload

---
+++ 5kyu/Moving Zeros To The End.js
@@ -0,0 +1,16 @@
+/*
+https://www.codewars.com/kata/moving-zeros-to-the-end/train/javascript
+
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+*/
+var moveZeros = function (arr) {
+    let arr_zeros = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            arr_zeros.push(+arr.splice(i, 1));
+            i--;
+        }
+    }
+    return arr.concat(arr_zeros);
+}
